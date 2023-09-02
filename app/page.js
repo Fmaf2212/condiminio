4e19e117e8d45a560d5f7fbe42f4761559870eed
fernandoMAF <avendanofrancia.moises@gmail.com>
2023-09-02 18:13:18 -0500

    Revert "subiendo pequeños cambios"
    This reverts commit 5e0f679a044cf0b96382340466b5e1bf4aa9f87a.

--- app/page.js
+++ app/page.js
@@ -2,7 +2,6 @@ import Header from "./header/page";
 import Slider from "./slider/page";
 
 import ticket from "../public/img/ticket.png"
-import iconSearch from "../public/img/iconSearch.png"
 import Image from "next/image";
 
 export default function Home() {
@@ -23,32 +22,14 @@ export default function Home() {
             <Slider images={images} />
           </div>
           <div className="flex justify-center">
-            <div className="flex gap-4 bg-white w-fit px-[17px] py-[16px] rounded-2xl">
-              <input type="text" placeholder="Qué buscas" className="border-b-[1px] border-[#F4AF14] leading-normal focus-visible:outline-none"/>
-              <button className="border-[1px] border-[#f4af14] p-2"><Image src={ticket} alt="ticket" /></button>
-              <button className="flex items-center gap-2 font-[MontserratBold] text-white rounded-xl pt-1 px-6 after:content:url('/img/iconSearch.png')" style={{background: "linear-gradient(180deg, rgba(207, 107, 0, 1) 0%, rgba(210, 113, 2, 1) 21.7%, rgba(219, 128, 6, 1) 49.47%, rgba(233, 155, 14, 1) 80.41%, rgba(244, 175, 20, 1) 100%)"}}><Image src={iconSearch} alt="iconSearch" /> Buscar</button>
+            <div className="bg-white w-fit">
+              <input type="text" placeholder="Qué buscas"/>
+              <button><Image src={ticket} alt="ticket" /></button>
+              <button>Buscar</button>
             </div>
           </div>
         </div>
       </section>
-      <main>
-        <h2>ANUNCIO DESTACADO</h2>
-        <span>Snacks y Licores</span>
-        <div>
-          <article>
-            {/* <Image /> */}
-            <h5>Licorería Pepito</h5>
-            <div>
-
-            </div>
-          </article>
-          <article></article>
-          <article></article>
-          <article></article>
-          <article></article>
-          <article></article>
-        </div>
-      </main>
     </main>
   )
 }
